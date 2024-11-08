3f24fc30eb78ee501ce7a86eee813f2639ab53dd
UnnitaYaduvanshi <unnitayaduvanshi@gmail.com>
2024-11-08 11:07:28 +0530

    functioning of search bar

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 console.log("Major Frontend tech used : HTML & CSS");
-console.log("Major Backend tech used : JavaScript & API Data - Request Handling");
+console.log("Major Backend tech used : JavaScript & API Integration");
+
+const city_name=document.querySelector('.city_name');
+const search=document.querySelector('.search');
+
+search.addEventListener('click',() => {
+    console.log(city_name.value)
+})
