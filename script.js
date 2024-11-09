437ff18b2f80a2c742d576b4e9eec07280b496c0
UnnitaYaduvanshi <unnitayaduvanshi@gmail.com>
2024-11-09 13:39:27 +0530

    code till now

--- script.js
+++ script.js
@@ -6,16 +6,24 @@ const searchKey = document.getElementById('.searchBttn');
 const weatherImg = document.querySelector('.images');
 const temper = document.querySelector('.temp');
 const explain = document.querySelector('.describe');
-const humidity = document.getElementById('.humid');
+const humid = document.getElementById('.humidit');
 const speed = document.getElementById('.wind-speed');
 
-async function identifyWeather(cityU){
+async function identifyWeather(city){
     const apiKey = 'b39a53c9458c52145b9a150c3394dd06';
-    const link = `https://api.openweathermap.org/data/2.5/weather?q=${cityU}&appid=${apiKey}`;
+    const link = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
  const weatherDetails = await fetch(link).then((response) => response.json());
 
-    console.log(weatherDetails);
+    console.log(weatherDetails)
+
+    temper.innerHTML = `${Math.round (weatherDetails.main.temp - 273.15)}°C`;
+
+    explain.innerHTML = `${weatherDetails.weather[0].description}`;
+
+    humid.innerHTML = `${weatherDetails.main.humidity}%`;
+
+    speed.innerHTML = `${weatherDetails.wind.speed}Km/H`;
 
 }
 searchBttn.addEventListener('click', () => {
