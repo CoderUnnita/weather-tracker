9b22171cdd33fc909d3f16a7e166bcf630c0f453
UnnitaYaduvanshi <unnitayaduvanshi@gmail.com>
2024-11-10 11:20:56 +0530

    errrrrrooorrr

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ console.log("Major Backend tech used : JavaScript & API");
 
 const city_name = document.querySelector('.city_name');
 const water = document.querySelector('.watai');
-const spread = document.querySelector('.wind');
-const searchBttn = document.querySelector('.searchBttn');
+const spread = document.querySelector('.winsp');
+const searchKey = document.querySelector('#searchBttn');
 const weatherImg = document.querySelector('images');
 const temper = document.querySelector('.temp');
 const explain = document.querySelector('.describe');
@@ -14,7 +14,18 @@ async function identifyWeather(city){
     const apiKey = 'b39a53c9458c52145b9a150c3394dd06';
     const link = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
- const weatherDetails = await fetch(link).then((response) => response.json());
+    const response = await fetch(link);
+
+    try{
+        if(!response.ok){
+            throw new Error(`HTTP error! status:${response.status}`);
+        }
+    } catch(error){
+        console.error(error);
+    }
+
+
+    const weatherDetails = await response.json();
 
     console.log(weatherDetails);
 
@@ -25,11 +36,11 @@ async function identifyWeather(city){
 
     temper.innerHTML = `${Math.round (weatherDetails.main.temp - 273.15)}°C`;
 
-    explain.innerHTML = `${weatherDetails.weather[0].description}`;
+    explain.innerHTML = weatherDetails.weather[0].description;
 
-    water.innerHTML = `${weatherDetails.main.humidity}`;
+    water.innerHTML = `${weatherDetails.main.humidity}%`;
 
-    spread.innerHTML = `${weatherDetails.wind.speed}`;
+    spread.innerHTML = `${weatherDetails.wind.speed}m/s`;
 
     switch(weatherDetails.weather[0].main)
     {
@@ -57,7 +68,18 @@ async function identifyWeather(city){
 
 }
 
-searchBttn.addEventListener('click', () => {
-    identifyWeather(city_name.value);
-})
+document.addEventListener('DOMContentLoaded', () => {
+    searchBttn.addEventListener('click', () => {
+      const city = city_name.value.trim();
+      if (city) {
+        identifyWeather(city);
+      } else {
+        console.log('Please enter a city name');
+      }
+    });
+  });
+
+// searchBttn.addEventListener('click', () => {
+//     identifyWeather(city_name.value);
+// })
 
